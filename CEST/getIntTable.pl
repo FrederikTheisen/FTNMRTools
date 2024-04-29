b539a77905c39bc2b8fda2c4a0dbaf550b477ff9
FrederikTheisen <FrederikTheisen@gmail.com>
2024-04-29 14:02:06 +0200

    Create getIntTable.pl

---
+++ CEST/getIntTable.pl
@@ -0,0 +1,3 @@
+# Bash script to extract assigned peak intensities from sparky peak list and HSQC slice
+# Script reads number of slices recorded, converts the sparky peaklist to nmr pipe and runs the seriesTab command to extract intensities of each peak
+
