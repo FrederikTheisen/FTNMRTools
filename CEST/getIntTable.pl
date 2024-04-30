db0c8c16815b869f6fad6aeea70c2bd5c57fd9a1
FrederikTheisen <FrederikTheisen@gmail.com>
2024-04-30 09:23:50 +0200

    Update getIntTable.pl

--- CEST/getIntTable.pl
+++ CEST/getIntTable.pl
@@ -1,7 +1,9 @@
 #!/bin/tcsh -f
-# Bash script to extract assigned peak intensities from sparky peak list and HSQC slice
+# Bash script to extract assigned peak intensities from sparky peak list and HSQC slice for CEST analysis using ChemEX
+# Frederik Theisen 2024
 # Script reads number of slices recorded, converts the sparky peaklist to nmr pipe and runs the seriesTab command to extract intensities of each peak
-# Place script in EXPNO folder
+# Place script in EXPNO folder. You spaky peak list should be name 'peaks_sparky'
+# seriesTab integration region '-dx' and '-dy' may be set to 3 if signal is weak, however this may increase effect of overlapping peaks
 
 set datadir = .
 
