5886e01e969de1fe2fa3c5aa96756a697f0d3471
FrederikTheisen <FrederikTheisen@gmail.com>
2024-04-30 09:12:59 +0200

    Update getIntTable.pl

--- CEST/getIntTable.pl
+++ CEST/getIntTable.pl
@@ -1,3 +1,15 @@
+#!/bin/tcsh -f
 # Bash script to extract assigned peak intensities from sparky peak list and HSQC slice
 # Script reads number of slices recorded, converts the sparky peaklist to nmr pipe and runs the seriesTab command to extract intensities of each peak
+# Place script in EXPNO folder
 
+set datadir = .
+
+# Make list of spectra
+find $datadir/data -name '*.ft2' | sort -n > spectra
+
+# Make NMR-pipe peak list from SPARKy peak list and NMR-pipe spectrum
+stPeakList.pl $datadir/data/test001.ft2 peaks_sparky > peaks.dat
+
+# Run seriesTab
+seriesTab -in peaks.dat -out peaks.ser -list spectra -dx 1 -dy 1
